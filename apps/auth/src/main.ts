e97fc98ea73b98e6db3be6d7294734f1cac26503
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-11-26 00:04:52 +0530

    module microservice created

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AUTH } from '@app/common';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../auth.proto'),
+        package: AUTH,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
